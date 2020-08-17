f7f3f78afc5fcf3d8cfce05228ed2776dd2e9f95
Kevin Powell Noumbissie <kevin-powell@hotmail.fr>
2020-08-17 15:48:53 +0200

    add custom button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -48,7 +49,7 @@ class SignIn extends React.Component {
             label='password'
             required
           />
-          <input type="submit" value="Submit Form"/>
+          <CustomButton type='submit'> SIGN IN</CustomButton>
         </form>
       </div>
     );
