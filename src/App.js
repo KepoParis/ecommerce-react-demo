75efcd6c355ac6135bd5f151a3d24fb226b099ab
Kevin Powell Noumbissie <kevin-powell@hotmail.fr>
2020-08-17 12:06:00 +0200

    add homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './homepage.component';
+
 function App() {
   return (
-    <div>Hello World</div>
+    <HomePage />
   );
 }
 
