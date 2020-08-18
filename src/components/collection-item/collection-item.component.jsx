19a151d4acf8c21bafeaa34d26107e04076143ca
Kevin Powell Noumbissie <kevin-powell@hotmail.fr>
2020-08-18 12:41:52 +0200

    add to cart button style

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './collection-item.styles.scss';
+import CustomButton from '../custom-button/custom-button.component';
 
 const CollectionItem = ({ imageUrl, name, price }) => (
   <div className='collection-item'>
@@ -14,6 +15,7 @@ const CollectionItem = ({ imageUrl, name, price }) => (
       <span className='name'>{name}</span>
       <span className='price'>{price}</span>
     </div>
+    <CustomButton inverted>Add to cart</CustomButton>
   </div>
 );
 
