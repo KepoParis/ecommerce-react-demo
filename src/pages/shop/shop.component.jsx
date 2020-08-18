9e771e5e3764223c25441e5b75e3e137e70bcc23
Kevin Powell Noumbissie <kevin-powell@hotmail.fr>
2020-08-18 17:48:55 +0200

    add directory and shop reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,31 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectShopCollections } from '../../redux/shop/shop.selectors.js';
 
-import SHOP_DATA from './shop.data.js';
-
-import './shop.styles.scss';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx';
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+import './shop.styles.scss';
 
-    this.state = {
-      collections: SHOP_DATA
+const ShopPage = ({ collections }) => (
+  <div className='shop-page'>
+    {
+      collections.map(({ id, ...otherCollectionProps}) => (
+        <CollectionPreview key={ id } { ...otherCollectionProps }/>
+      ))
     }
-  }
+  </div>
+);
 
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className='shop-page'>
-        {
-          collections.map(({ id, ...otherCollectionProps}) => (
-            <CollectionPreview key={ id } { ...otherCollectionProps }/>
-          ))
-        }
-      </div>
-    );
-  }
-}
+const mapStateToProps = createStructuredSelector({
+  collections: selectShopCollections
+});
 
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
